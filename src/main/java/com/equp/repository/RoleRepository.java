9f5f541acd685053078d8c775d3904f479e674f5
pamishenko <pamishenko@gmail.com>
2020-10-12 00:31:55 +0300

    очистил папку

--- src/main/java/com/equp/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.equp.repository;
-
-import com.equp.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/equp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.equp.repository;
-
-import com.equp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
