c5314c484ea058762d92384b3a4f5130c48d1825
Roman Ungefuk <roman.ungefuk@gmail.com>
2021-01-15 18:42:08 +0300

    Произведено слияние веток main с jwt. Методы работают по jwt-токенам.

--- src/main/java/com/equp/back/backend/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/equp/back/backend/security/jwt/JwtTokenProvider.java
@@ -81,8 +81,8 @@ public class JwtTokenProvider {
 
     public String resolveToken(HttpServletRequest req) {
         String bearerToken = req.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer_")) {
-            return bearerToken.substring(7, bearerToken.length());
+        if (bearerToken != null && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(6, bearerToken.length());
         }
         return null;
     }
--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -2,6 +2,7 @@ INSERT INTO  roles(name) VALUES
 ('ROLE_USER'),
 ('ROLE_ADMIN');
 
+-- 12345
 INSERT INTO  users VALUES
 (1, 'user', 'Default newUser','Default newUser','user@mail.ru','$2a$04$sweeYfA1IOmERGq0mXFjXeY7ksEK.7h0AHgj0yM84XSOICobdw9nC', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
 (2, 'pamishenko', 'Pavel','Mischenko','pamishenko@gmail.com','$2a$04$TOWdB3.7NVUhKNPS/LE6ze6OBG.slIGlVTlaa0iQL0ZhfpB2BUm/q', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
@@ -11,6 +12,7 @@ INSERT INTO  users VALUES
 (6, 'jvorobjeva', 'Julia','Vorobjeva','J.vorobjeva@mail.ru','$2a$04$IvfulGd/fwPOtGCrjlkMWOby1ecmjFSCckScGS5Gee7WcoDV8dZnG', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
 (7, 'manimusic', 'Mani','Music','Manimusic@yandex.ru','$2a$04$A2ISF3jbBccMuHkWaTyiLOYrbHfhUQ0wgLmrXRAsJgC8OD8tYZ8wm', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
 (8, 'romanungefuk', 'Roman','Ungefuk','roman.ungefuk@gmail.com','$2a$04$yrw4nT/LZzRJeiptGHGuKO4rhhb.LTzakr0MayNLrgW.T55iYEdNq', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE');
+
 
 INSERT INTO  user_roles VALUES
 (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1), (8,2);
