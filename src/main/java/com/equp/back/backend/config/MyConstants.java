186912f1b911300121bdcf9d9b60ef97854a50ff
pamishenko <pamishenko@gmail.com>
2021-01-16 23:52:51 +0300

    Небольшие правки.В pom.xml удалил зависимость spring security. Она мешала.

--- src/main/java/com/equp/back/backend/config/MyConstants.java
+++ src/main/java/com/equp/back/backend/config/MyConstants.java
@@ -2,8 +2,8 @@ package com.equp.back.backend.config;
 public class MyConstants {
 
     // Replace with your email here:
-    public static final String MY_EMAIL = "4equpp@gmail.com";
-//    public static final String MY_EMAIL = "admin@eq-up.ru";
+//    public static final String MY_EMAIL = "4equpp@gmail.com";
+    public static final String MY_EMAIL = "support@eq-up.ru";
     // Replace password!!
     public static final String MY_PASSWORD = "1Qwerty@1";
 //    public static final String MY_PASSWORD = "KH4Qab0W8oNN";
