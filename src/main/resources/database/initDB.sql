7691a37280b6c1676dbfc14b476d906e0b4f8c7e
Roman Ungefuk <roman.ungefuk@gmail.com>
2021-01-20 22:41:17 +0300

    Поправлена таблица users поле name не должно быть уникальным.

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id bigserial, PRIMARY KEY(id),
-    name  VARCHAR(200) NOT NULL unique,
+    name  VARCHAR(200) NOT NULL,
     first_name  VARCHAR(200) NOT NULL ,
     last_name  VARCHAR(200) NOT NULL ,
     email VARCHAR(254) NOT NULL unique,
