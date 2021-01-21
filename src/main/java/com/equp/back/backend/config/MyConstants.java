1af4f039c39419e9e6cad0da4205b55892d0e703
Pavel <pamishenko@gmail.com>
2021-01-21 21:18:13 +0300

    Сменил пароль от почты

--- src/main/java/com/equp/back/backend/config/MyConstants.java
+++ src/main/java/com/equp/back/backend/config/MyConstants.java
@@ -2,6 +2,6 @@ package com.equp.back.backend.config;
 public class MyConstants {
 
     public static final String MY_EMAIL = "no-reply@eq-up.ru";
-    public static final String MY_PASSWORD = "E48dfa8c056";
+    public static final String MY_PASSWORD = "E48dfa8c";
 
 }
