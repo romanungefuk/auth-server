c7835bd03fe0512c8ec84dc0884573862189f7c5
Roman Ungefuk <roman.ungefuk@gmail.com>
2021-02-07 21:45:16 +0300

    Изменены конфигурационные файлы для работы с базой mariadb и файл initDB.sql.

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id bigserial, PRIMARY KEY(id),
+    id BIGINT AUTO_INCREMENT, PRIMARY KEY(id),
     name  VARCHAR(200) NOT NULL,
     first_name  VARCHAR(200) NOT NULL ,
     last_name  VARCHAR(200) NOT NULL ,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS roles
 (
-    id    BIGSERIAL PRIMARY KEY,
+    id    BIGINT AUTO_INCREMENT,PRIMARY KEY(id),
     name  VARCHAR(100) NOT NULL unique,
     created TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -21,12 +21,12 @@ CREATE TABLE IF NOT EXISTS roles
 );
 
 CREATE TABLE IF NOT EXISTS user_roles ( user_id BIGINT NOT NULL, role_id BIGINT NOT NULL, primary key (user_id, role_id),
-                                 FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (role_id) REFERENCES roles (id) );
+                                        FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (role_id) REFERENCES roles (id) );
 
 
 CREATE TABLE IF NOT EXISTS experiences
 (
-    id bigserial, PRIMARY KEY(id),
+    id BIGINT AUTO_INCREMENT, PRIMARY KEY(id),
     start_location DOUBLE PRECISION NOT NULL,
     mindfulness  DOUBLE PRECISION NOT NULL ,
     attitudes  DOUBLE PRECISION NOT NULL ,
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS experiences
 
 CREATE TABLE IF NOT EXISTS test_result
 (
-    id bigserial, PRIMARY KEY(id),
+    id BIGINT AUTO_INCREMENT, PRIMARY KEY(id),
     start_location DOUBLE PRECISION NOT NULL,
     mindfulness  DOUBLE PRECISION NOT NULL ,
     attitudes  DOUBLE PRECISION NOT NULL ,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS test_result
 
 CREATE TABLE IF NOT EXISTS moods
 (
-    id bigserial, PRIMARY KEY(id),
+    id BIGINT AUTO_INCREMENT, PRIMARY KEY(id),
     user_id bigint REFERENCES users(id),
     date bigint default 0 not null,
     mood int not null,
