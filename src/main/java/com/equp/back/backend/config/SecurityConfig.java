32a02461f07d9d4c33779778487f365f91f8ec86
rungefuk <roman.ungefuk@gmail.com>
2021-03-19 01:26:18 +0300

    Добавил логов.

--- src/main/java/com/equp/back/backend/config/SecurityConfig.java
+++ src/main/java/com/equp/back/backend/config/SecurityConfig.java
@@ -18,13 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 private final JwtTokenProvider jwtTokenProvider;
 
     private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
-    private static final String LOGIN_ENDPOINT = "/api/v1/auth**";
+    private static final String LOGIN_ENDPOINT = "/api/v1/auth";
     private static final String SIGNUP_ENDPOINT = "/api/v1/user/signup";
     private static final String UPDATEBYEMAIL_ENDPOINT = "/api/v1/user/update-by-mail";
     private static final String PASSWORDCHANGE_ENDPOINT = "/password_change**";
     private static final String USER_ENDPOINT = "/api/v1/user**";
     private static final String MOOD_ENDPOINT = "/api/v1/mood**";
-    //private static final String CHECK_ENDPOINT = "/api/v1/auth/check";
+    private static final String CHECK_ENDPOINT = "/api/v1/auth/**";
 
     @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
@@ -52,7 +52,7 @@ private final JwtTokenProvider jwtTokenProvider;
                 .antMatchers(SIGNUP_ENDPOINT).permitAll()
                 .antMatchers(UPDATEBYEMAIL_ENDPOINT).permitAll()
                 .antMatchers(PASSWORDCHANGE_ENDPOINT).permitAll()
-                //.antMatchers(CHECK_ENDPOINT).permitAll()
+                .antMatchers(CHECK_ENDPOINT).permitAll()
                 //.antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
                 .antMatchers(USER_ENDPOINT).hasRole("USER")
                 .antMatchers(MOOD_ENDPOINT).hasRole("USER")
