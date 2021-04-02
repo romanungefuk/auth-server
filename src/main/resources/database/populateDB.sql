50b9b8e0f0bfa1135b338f65169293584434990b
pamishenko <pamishenko@gmail.com>
2021-04-02 19:11:26 +0300

    обновил инициализацией тестового юзера id  c 17179  на 9

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -12,7 +12,7 @@ INSERT INTO  users VALUES
 (6, 'jvorobjeva', 'Julia','Vorobjeva','j.vorobjeva@mail.ru',null,'$2a$04$IvfulGd/fwPOtGCrjlkMWOby1ecmjFSCckScGS5Gee7WcoDV8dZnG', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
 (7, 'manimusic', 'Mani','Music','manimusic@yandex.ru',null,'$2a$04$A2ISF3jbBccMuHkWaTyiLOYrbHfhUQ0wgLmrXRAsJgC8OD8tYZ8wm', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE'),
 (8, 'romanungefuk', 'Roman','Ungefuk','roman.ungefuk@gmail.com',null,'$2a$04$yrw4nT/LZzRJeiptGHGuKO4rhhb.LTzakr0MayNLrgW.T55iYEdNq', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE');
-(17179, 'testuser', 'Test','User','testuser@icloud.com',null,'$2y$12$P5aOczmzIpLg.fkwB2TEvu/gskl1ZhL3FXwsps8fOGmRBhmJx8Pva', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE');
+(9, 'testuser', 'Test','User','testuser@icloud.com',null,'$2y$12$P5aOczmzIpLg.fkwB2TEvu/gskl1ZhL3FXwsps8fOGmRBhmJx8Pva', '2020-12-10 00:38:25', '2020-12-10 00:38:31','ACTIVE');
 
 INSERT INTO  user_roles VALUES
 (1,1),(2,2),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1), (8,2), (17179,1);
